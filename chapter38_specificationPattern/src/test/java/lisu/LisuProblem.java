c7254b1172019494a8de9f70c2af23cb5fedba2a
yghtest <948170910@qq.com>
2017-08-17 19:32:19 +0800

    co

---
+++ chapter38_specificationPattern/src/test/java/lisu/LisuProblem.java
@@ -0,0 +1,9 @@
+package lisu;
+
+public class LisuProblem {
+	public static void main(String[] args) {
+		float number1 = 45.56f;
+		int number2 = 152;
+		System.out.println("the sum is " + number1 + number2);
+	}
+}
